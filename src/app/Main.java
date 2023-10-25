9efc1ba158094963597f61cb488ac5e5c2470323
xMAXSIMKAx <maxymram@gmail.com>
2023-10-25 21:45:10 +0200

    Change commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,6 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }
